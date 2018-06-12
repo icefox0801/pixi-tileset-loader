dca8c431aa6c455ef472035c776a5f2cb6097523
icefox0801 <icefox0801@hotmail.com>
2018-06-12 16:24:10 +0800

    kick off

--- README.md
+++ README.md
@@ -1,2 +1,117 @@
 # pixi-tileset-loader
-Webpack loader for generate tilesets for PIXI.js
+
+> GitHub: https://github.com/icefox0801/pixi-tileset-loader
+
+Webpack loader for generating tilesets for PIXI.js
+
+## Install
+`npm install tinyjs-resource-loader -D`
+
+## Usage
+1. Create a `.tileset` (or other filename) file in frames directory
+```bash
+animation
+├── .tileset
+├── 001.png
+├── 002.png
+└── 003.png
+```
+2. See [Image processing params](#Image%20process%20params) to configure `.tileset` in `yaml` format
+```yaml
+skip: 1
+colors: 16
+scale: 0.5
+```
+3. Add `pixi-tileset-loader` in `webpack.config.js`:
+```javascript
+module.export = {
+  // statements
+  module: {
+    loaders: [
+      {
+        test: /\.tileset/i,
+        loader: 'tinyjs-resource-loader',
+        query: {
+          output: 'game/images',
+          image: { // url-loader for PNG file
+            name: 'resources/[name].[ext]',
+            limit: 4096
+          },
+          json: { // url-loader for JSON file
+            name: 'resources/[name].[ext]',
+            limit: 1
+          }
+        }
+      }
+    ]
+  },
+  output: {
+    path: path.resovle('dist')
+  }
+};
+```
+4. `import` or `require` the `.tileset` file in your module
+```javascript
+import tilesetAnimationJSON from './frames/animation/.tileset';
+// Will get a JSON file path in return，and the image path will be replaced with resources/[name].[ext] in JSON file
+```
+
+## Processing
+1. Frames trimming：to specific a `skip` option to perform a *N to 1* frame trimming
+2. Spritesheet：genarate PNG and JSON file by [spritesheet.js](https://github.com/krzysztof-o/spritesheet.js)
+3. Image optimizing：use [node-pngquant](https://github.com/papandreou/node-pngquant) to reduce colors amount
+4. Write the PNG and JSON file in `game/images` directory (specified by `query.output`)
+```bash
+game
+├── frames
+│   ├── animation # the frames directory
+│   │   ├── .tileset
+│   │   ├── 001.png
+│   │   ├── 002.png
+│   │   └── 003.png
+├── images # the directory to cache the PNG and JSON file
+│   ├── tileset-animation.json
+│   └── tileset-animation.png
+└── resources.js
+```
+5. Build PNG and JSON file in `game/images` directory into `dist/resources` (specified by `output.path` in webpack config) by [url-loader](https://github.com/webpack-contrib/url-loader). This will replace `meta.image` in JSON with `publicPath` or `base64` (depends on `query.image`)
+```bash
+dist
+└── resources
+    ├── tileset-animation.json
+    └── tileset-animation.png
+```
+
+## System dependencies
+First to ensure these package installed in system:
++ [ImageMagick](https://www.imagemagick.org/script/download.php): `identify` and `convert` command are required to get image information and resize image
++ [pngquant](https://pngquant.org/)：`pngquant` command is required to reduce colors of PNG image
+
+
+## Options
++ `query.output`: the directory to cache PNG and JSON, we recommend specifying a source code directory and commit this directory as weel. The cache will be disabled when specified as empty string.
++ `query.loader`: use `url-loader`, `json-loader` to process JSON file, or not process. Default is `url`, `json`、`none` optional.
++ `query.process`: to process frames or not. It will directly read from the directory `output` option specified to perform the build when `false`
++ `query.image`: [url-loader](https://github.com/webpack-contrib/url-loader) webpack loader options for PNG file
++ `query.json`: [url-loader](https://github.com/webpack-contrib/url-loader) webpack loader options for JSON file. Not needed when `query.loader` specified `json`.
++ `query.verbose`: log the entire error stack or not.
+
+> When `query.process` is specified `false`: the first 4 steps will be ignored, PNG and JSON files will be directly read from the directory `query.output` specified, and webpack will emit these files via [url-loader](https://github.com/webpack-contrib/url-loader) , but a **webpack warning** will be emitted as well. This ensure the build in remote server, where ImageMagick or pngquant package is missing.
+
+## Image processing params
++ `trim`：trim the whitespace in image or not, default is `false`
++ `scale`: the scale value, based on [imagemagick-stream](https://github.com/eivindfjeldstad/imagemagick-stream) to resize the PNG image, default is `1`
++ `padding`: padding in the spritesheet, default is `10`
++ `skip`：skip *N* frames in every *N + 1* frames, default is `0`
++ `colors`：colors amount for pngquant, default is `0`
++ `files`: file paths in `[path]-[name]` format, frames will be read from the directory `files` specified instead of the directory where `.tileset` is in
++ `excludes`: the file paths to exclude
++ `interpolate`: string template to specify the prefix, such as `$name$-tileset` and `tileset`
+
+`files` specifies the paths relative to the directory where `.tileset` is in, for example:
+```yaml
+files:
+  ../animation-a/001.png: animation-a
+  ../animation-b/001.png: animation-b
+  ../animation-c/001.png: animation-c
+```
---
+++ index.js
@@ -0,0 +1,296 @@
+var path = require('path');
+var fs = require('graceful-fs');
+var fse = require('node-fs-extra');
+var yaml = require('yaml-js');
+var tempfile = require('tempfile');
+var loaderUtils = require('loader-utils');
+var Promise = require('bluebird');
+var exec = require('child_process').exec;
+var PngQuant = require('pngquant');
+var urlLoader = require('url-loader');
+var jsonLoader = require('json-loader');
+var im = require('imagemagick-stream');
+
+var spritesheetJS = path.resolve(__dirname, 'node_modules/spritesheet-js/index.js');
+
+function zeroPad (num, n) {
+  return String('00000' + num).slice(-n);
+};
+
+function copyFile (source, dest, scale) {
+  return new Promise(function (resolve) {
+    var readStream = fs.createReadStream(source);
+    var writeStream = fs.createWriteStream(dest);
+
+    if (scale && scale < 1) {
+      var percentage = `${parseInt(scale * 100, 10)}%`;
+      var resize = im().resize(percentage);
+      readStream.pipe(resize).pipe(writeStream);
+    } else {
+      readStream.pipe(writeStream);
+    }
+
+    writeStream.on('finish', function () {
+      resolve();
+    });
+  });
+}
+
+// trim frames
+function trimFrames (name, source, dest, config) {
+  var files = [];
+  var excludes = [];
+
+  if (config.excludes && config.excludes.length) {
+    excludes = config.excludes.map(function (exclude) {
+      return path.resolve('/', exclude);
+    });
+  }
+
+  if (config.files) {
+    files = Object.keys(config.files);
+  } else {
+    files = fs.readdirSync(source);
+  }
+
+  fs.mkdirSync(dest);
+
+  files = files.filter(function (file) {
+    var match = ~excludes.indexOf(path.resolve('/', file));
+    return path.extname(file) === '.png' && !match;
+  });
+
+  var promises = [];
+
+  files.forEach(function (file, index) {
+    var factor = config.skip + 1;
+
+    if (index % factor !== 0) return false;
+
+    var filePath = path.join(source, file);
+    var fileIndex, filename;
+
+    if (config.files) {
+      filename = `${name}-${config.files[file]}.png`;
+    } else {
+      fileIndex = zeroPad(parseInt(index / factor + 1, 10), 3);
+      filename = config.skip ? `${name}-${fileIndex}.png` : `${name}-${file}`;
+    }
+
+    promises.push(copyFile(filePath, path.join(dest, filename), config.scale));
+  });
+
+  return Promise.all(promises);
+}
+
+// spritesheet
+function spritesheet (name, input, output, config) {
+  var args = [
+    '-f',
+    'json',
+    '-p',
+    output,
+    '-n',
+    name,
+    '--trim',
+    config.trim,
+    '--padding',
+    config.padding
+  ];
+
+  return new Promise(function (resolve, reject) {
+    exec(`node ${spritesheetJS} ${args.join(' ')} ${input}`, function (error, stdout, stderr) {
+      if (error) return reject(error);
+
+      if (stderr) return reject(stderr);
+
+      var jsonPath = path.join(output, `${name}.json`);
+      var oldJSON = fs.readFileSync(jsonPath);
+      var newJSON = sortJSONFrames(oldJSON);
+      fs.writeFileSync(jsonPath, newJSON, 'utf-8');
+      resolve(stdout);
+    });
+  });
+}
+
+// png optimize
+function pngOptimize (source, dest, colors) {
+  return new Promise(function (resolve, reject) {
+    var readStream = fs.createReadStream(source);
+    var writeStream = fs.createWriteStream(dest);
+
+    if (colors) {
+      var pngquant = new PngQuant([colors]);
+      readStream.pipe(pngquant).pipe(writeStream);
+    } else {
+      readStream.pipe(writeStream);
+    }
+
+    readStream.on('error', function (error) {
+      reject(error);
+    });
+    writeStream.on('finish', function () {
+      resolve();
+    });
+  });
+}
+
+function sortJSONFrames (content) {
+  var json = JSON.parse(content);
+  var oldFrames = json.frames;
+  var newFrames = {};
+  var frameIds = Object.keys(oldFrames).sort();
+
+  frameIds.forEach(function (frameId) {
+    newFrames[frameId] = oldFrames[frameId];
+  });
+
+  json.frames = newFrames;
+  return JSON.stringify(json, null, 2);
+}
+
+function rewriteJSON (content, imagePathStr) {
+  var sheetConfig = JSON.parse(content);
+  var imagePath = /"([^"]+)"/.exec(imagePathStr)[1];
+  sheetConfig.meta.image = imagePath;
+  return JSON.stringify(sheetConfig);
+}
+
+function buildFiles (context, query, options = {}, name) {
+  var content = '';
+  if (query.loader === 'none') {
+    return content;
+  }
+  // build image
+  var imageFullPath = path.resolve(query.output, `${name}.png`);
+  var imageContent = fs.readFileSync(imageFullPath);
+  var imageContext = Object.create(context);
+  imageContext.resourcePath = imageFullPath;
+  imageContext.query = query.image;
+  imageContext.options = options;
+  var imagePathStr = urlLoader.call(imageContext, imageContent);
+  // build json
+  var jsonFullPath = path.resolve(query.output, `${name}.json`);
+  var jsonStr = fs.readFileSync(jsonFullPath);
+  var jsonContent = rewriteJSON(jsonStr, imagePathStr);
+  var jsonContext = Object.create(context);
+  jsonContext.resourcePath = jsonFullPath;
+  jsonContext.query = query.json;
+  jsonContext.options = options;
+
+  if (query.loader === 'json') {
+    content = jsonLoader.call(jsonContext, jsonContent);
+  } else {
+    content = urlLoader.call(jsonContext, jsonContent);
+  }
+
+  return content;
+}
+
+module.exports = function (content) {
+  var self = this;
+  var callback = self.async();
+  var query = loaderUtils.getOptions(self) || {};
+  var config = yaml.load(content.toString()) || {};
+  config = Object.assign({}, defaults, config);
+
+  var parsed = path.parse(self.context);
+  var name = `tileset-${parsed.name}`;
+  var inputTemp = tempfile();
+  var outputTemp = tempfile();
+
+  if (typeof query.process === 'undefined') query.process = true;
+
+  if (!query.output) query.output = inputTemp;
+
+  if (config.interpolate) name = config.interpolate.replace('$name$', name);
+
+  self.cacheable(true);
+  self.addContextDependency(self.context);
+
+  if (config.files) {
+    Object.keys(config.files).forEach(function (filePath) {
+      var fullPath = path.resolve(self.context, filePath);
+      self.addDependency(fullPath);
+    });
+  }
+  // process option disabled
+  if (!query.process) {
+    var result = '';
+    var imageFullPath = path.resolve(query.output, `${name}.png`);
+    if (!fs.existsSync(imageFullPath)) {
+      self.emitError(`${name}.json and ${name}.png are not found in the directory ouput option specified when process option is disabled, please ensure these files were built into this directory in the last build.`);
+    } else {
+      self.emitWarning(`Image processing will not execute when process option is disabled. ${name}.json and ${name}.png will be read from the directory ouput option specified.`);
+      result = buildFiles(self, query, self.options, name);
+    }
+
+    process.nextTick(function () {
+      fse.remove(inputTemp);
+      fse.remove(outputTemp);
+    });
+
+    return callback(null, result);
+  }
+
+  trimFrames(name, self.context, inputTemp, config)
+    .then(function () {
+      var source = path.join(inputTemp, '*.png');
+      return spritesheet(name, source, outputTemp, config);
+    })
+    .then(function () {
+      if (!fs.existsSync(query.output)) {
+        const err = fs.mkdirSync(query.output, 0o777);
+        if (err) {
+          throw err;
+        }
+      }
+      var source = path.join(outputTemp, `${name}.png`);
+      var dest = path.resolve(query.output, `${name}.png`);
+      return pngOptimize(source, dest, config.colors);
+    })
+    .then(function () {
+      var source = path.join(outputTemp, `${name}.json`);
+      var dest = path.resolve(query.output, `${name}.json`);
+
+      fse.copy(source, dest, function () {
+        setTimeout(function () {
+          var content = buildFiles(self, query, self.options, name);
+          process.nextTick(function () {
+            fse.remove(inputTemp);
+            fse.remove(outputTemp);
+          });
+          callback(null, content);
+        }, 500);
+      });
+    })
+    .catch(function (error) {
+      if (query.verbose) {
+        console.error(error);
+      }
+
+      if (query.process) {
+        self.emitError(`Error occured in image processing, ImageMagick or pngquant may not be correctly installed or specified is operating system. See https://github.com/ant-tinyjs/tinyjs-resource-loader#%E7%B3%BB%E7%BB%9F%E4%BE%9D%E8%B5%96 for more information.`);
+      }
+
+      var content = buildFiles(self, query, self.options, name);
+      process.nextTick(function () {
+        fse.remove(inputTemp);
+        fse.remove(outputTemp);
+      });
+      callback(null, content);
+    });
+};
+
+module.exports.raw = true;
+
+var defaults = {
+  interpolate: '',
+  trim: false,
+  scale: 1,
+  padding: '10',
+  colors: 0,
+  skip: 0,
+  files: null,
+  excludes: []
+};
